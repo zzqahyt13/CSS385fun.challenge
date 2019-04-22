599eb566408e640448f18728e9a3ea4d40f0f38b
zzqahyt13 <zzqahyt13@gmail.com>
2019-04-22 13:39:10 -0700

    initial project

---
+++ Fun challenge/Assets/Door.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header(" connect to a scene")]
+    public string goToTheScene;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(goToTheScene);
+        }
+        
+    }
+}
---
+++ Fun challenge/Assets/Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    public float forwardForce = 10f;
+    public float upwardForce = 30f;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(transform.right* forwardForce);
+        if(Input.GetKey("space"))
+        {
+            rb.AddForce(transform.up * upwardForce);
+        }
+    }
+}
